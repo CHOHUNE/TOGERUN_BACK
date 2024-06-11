97f5574594a6adb7c29d7fb39cd22d5b4bebfb67
CHOHUNE <yuureru@gmail.com>
2024-06-11 19:52:07 +0900

    feat: post etc

--- src/main/java/com/example/simplechatapp/entity/Post.java
+++ src/main/java/com/example/simplechatapp/entity/Post.java
@@ -2,21 +2,39 @@ package com.example.simplechatapp.entity;
 
 
 import jakarta.persistence.*;
-import lombok.Data;
-import lombok.Getter;
+import lombok.*;
 
-@Data
+import java.time.LocalDate;
+
+@Getter
 @Entity
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false,length = 100)
     private String title;
     private String content;
 
     @ManyToOne
     @JoinColumn(name="user_id")
     private User user;
+
+    private LocalDate localDate;
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
+
+    public void changeContent(String content) {
+        this.content = content;
+    }
+
 
 }
