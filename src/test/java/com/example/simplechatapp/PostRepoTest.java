034ffc587badc3341246ee6917a8c9af9c32d66e
CHOHUNE <yuureru@gmail.com>
2024-06-17 17:09:53 +0900

    test : post repo 이미지 추가 테스트

--- src/test/java/com/example/simplechatapp/PostRepoTest.java
+++ src/test/java/com/example/simplechatapp/PostRepoTest.java
@@ -2,6 +2,7 @@ package com.example.simplechatapp;
 
 import com.example.simplechatapp.entity.Post;
 import com.example.simplechatapp.repository.PostRepository;
+import com.example.simplechatapp.util.CustomFileUtil;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +17,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @SpringBootTest
 @Log4j2
@@ -26,6 +28,8 @@ class PostRepoTest {
 
     @Autowired
     private  PostRepository postRepository;
+    @Autowired
+    private CustomFileUtil customFileUtil;
     // test 환겨에서는 주로 필드 주입을 사용한다.
 
     @Test
@@ -75,6 +79,24 @@ class PostRepoTest {
 
         log.info(result.getContent());
         log.info(result.getTotalElements());
+
+    }
+
+    @Test
+    @DisplayName("이미지 업로드 테스트")
+    public void test5() {
+
+        Post post = Post.builder().title("이미지 저장 게시물").content("이미지 저장 컨텐츠").build();
+
+
+
+
+
+        post.addImageString(UUID.randomUUID()+"_"+"IMG.JPG");
+        post.addImageString(UUID.randomUUID()+"_"+"IMG2.JPG");
+        post.addImageString(UUID.randomUUID()+"_"+"IMG3.JPG");
+
+        postRepository.save(post);
 
     }
 
