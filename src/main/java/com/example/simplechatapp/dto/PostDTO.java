487c908151317783276e87d5673575c7d5fba938
CHOHUNE <yuureru@gmail.com>
2024-06-17 17:10:09 +0900

    fix : etc

--- src/main/java/com/example/simplechatapp/dto/PostDTO.java
+++ src/main/java/com/example/simplechatapp/dto/PostDTO.java
@@ -29,7 +29,11 @@ public class PostDTO {
 
         private boolean delFlag;
 
+
+        @Builder.Default // Builder.Default 를 사용하면 초기화한 상태를 직접 설정할 수 있다.
+        // 가령 List 를 초기화 하지 않으면 null 값이 들어가고, nullPointException 등이 발생될 수 있다.
         private List<MultipartFile> file = new ArrayList<>();
+        @Builder.Default
         private List<String> uploadFileName = new ArrayList<>();
 
 
--- src/main/java/com/example/simplechatapp/util/CustomFileUtil.java
+++ src/main/java/com/example/simplechatapp/util/CustomFileUtil.java
@@ -5,22 +5,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.UUID;
 
 @Log4j2
 @RequiredArgsConstructor
 @Component
-public class customFileUtil {
+public class CustomFileUtil {
 
-    @Value("${example.simplechatapp.upload.path}")
+
+
+    @Value("${com.example.simplechatapp.upload.path}")
     private String uploadPath;
 
     @PostConstruct
