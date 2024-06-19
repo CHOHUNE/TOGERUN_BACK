2a9bdecffb7ac28ec835e2c6a4f31a67aacc784d
CHOHUNE <yuureru@gmail.com>
2024-06-19 13:39:33 +0900

    feat : memberRole 추가, UserRepo getWithRole 메서드 추가( 한방쿼리 )
    entityGraph 로 N+1 방지,

--- src/main/java/com/example/simplechatapp/entity/MemberRole.java
+++ src/main/java/com/example/simplechatapp/entity/MemberRole.java
@@ -1,4 +1,6 @@
 package com.example.simplechatapp.entity;
 
 public enum MemberRole {
+
+        USER,MANAGER,ADMIN
 }
--- src/main/java/com/example/simplechatapp/repository/UserRepository.java
+++ src/main/java/com/example/simplechatapp/repository/UserRepository.java
@@ -1,8 +1,18 @@
 package com.example.simplechatapp.repository;
 
 import com.example.simplechatapp.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public
 interface UserRepository extends JpaRepository<User,Long> {
+
+    @EntityGraph(attributePaths = "memberRoleList")
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    User getWithRole(@Param("email")String email);
+
+
+
 }
