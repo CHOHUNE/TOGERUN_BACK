b9417240cdca62596a579ae481a6219c1e61b993
CHOHUNE <yuureru@gmail.com>
2024-08-01 13:58:14 +0900

    fix : User -> createdBy 로 수정
    소규모 프로젝트 이기 때문에 무결성 보다는 구현과 기능 중심으로 변경. (추후에 또 바꿀 수도 있음)

--- src/main/java/com/example/simplechatapp/entity/Comment.java
+++ src/main/java/com/example/simplechatapp/entity/Comment.java
@@ -1,17 +1,17 @@
 package com.example.simplechatapp.entity;
 
-
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
-@Table(name = "comment")
-@NoArgsConstructor(access = AccessLevel.PROTECTED) //access = AccessLevel.PROTECTED : 생성자 접근 제한
+//@Table(name = "comment") 이름이 같은 경우 생략 가능
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
     @Id
@@ -23,12 +23,18 @@ public class Comment {
     @JoinColumn(name = "post_id")
     private Post post;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+    private String createdBy;
+
+    // 소규모 프로젝트, 성능을 중시하는 경우에 단순히 createdBy를 사용해도 무방
+    // 대규모 프로젝트, 무결성을 중시하는 겨우에는 User 객체를 사용하는 것이 좋음
+    // 둘 중 뭘 쓸지는 고민 필요..
 
     @Column(nullable = false)
-    @Lob // 긴 길이의 문자열을 저장하기 위한 어노테이션
+    @Lob
     private String content;
 
     private boolean delFlag;
@@ -37,16 +43,20 @@ public class Comment {
     @JoinColumn(name = "parent_id")
     private Comment parent;
 
-
+//    @Builder.Default
     @OneToMany(mappedBy = "parent", fetch = FetchType.LAZY)
     private List<Comment> children = new ArrayList<>();
 
-    public void updateParent(Comment parent) {
+    private LocalDateTime createdAt;
+
+    @Builder
+    public Comment(Post post, String createdBy, String content, Comment parent) {
+        this.post = post;
+        this.createdBy = createdBy;
+        this.content = content;
         this.parent = parent;
+        this.createdAt = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
     }
 
-    public boolean validateUser(User user) {
-        return !this.user.equals(user);
-    }
 
 }
