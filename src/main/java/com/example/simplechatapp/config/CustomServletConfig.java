84dcd90d4a04cf1c98e0bb7cfaab22cc8f6d2cc8
CHOHUNE <yuureru@gmail.com>
2024-08-19 14:39:50 +0900

    feat : addFormatter 에 LocalDateTimeFormatter 전역 으로 String 변환하는 포매터 추가

--- src/main/java/com/example/simplechatapp/config/CustomServletConfig.java
+++ src/main/java/com/example/simplechatapp/config/CustomServletConfig.java
@@ -3,15 +3,18 @@ package com.example.simplechatapp.config;
 
 import com.example.simplechatapp.controller.formatter.LocalDateFormatter;
 //import com.example.simplechatapp.security.resolver.CustomArgumentResolver;
+import com.example.simplechatapp.controller.formatter.LocalDateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Configuration
@@ -23,6 +26,9 @@ public class CustomServletConfig implements WebMvcConfigurer {
 
 
         registry.addFormatter(new LocalDateFormatter());
+        registry.addFormatter(new LocalDateTimeFormatter());
+
+
 
     }
 
---
+++ src/main/java/com/example/simplechatapp/controller/formatter/LocalDateTimeFormatter.java
@@ -0,0 +1,23 @@
+package com.example.simplechatapp.controller.formatter;
+
+import org.springframework.format.Formatter;
+
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class LocalDateTimeFormatter implements Formatter<LocalDateTime> {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+
+    @Override
+    public LocalDateTime parse(String text, Locale locale) throws ParseException {
+        return LocalDateTime.parse(text, formatter);
+    }
+
+    @Override
+    public String print(LocalDateTime object, Locale locale) {
+        return formatter.format(object);
+    }
+}
