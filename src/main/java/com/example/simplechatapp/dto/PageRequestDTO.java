c9290d543b5b64e886d26663f7dc227d76bdebd5
CHOHUNE <yuureru@gmail.com>
2024-10-08 01:26:01 +0900

    refactor : 페이지 사이즈 10->9

--- src/main/java/com/example/simplechatapp/dto/PageRequestDTO.java
+++ src/main/java/com/example/simplechatapp/dto/PageRequestDTO.java
@@ -17,7 +17,7 @@ public class PageRequestDTO {
     private int page = 1;
 
     @Builder.Default
-    private int size = 10;
+    private int size = 9;
 
     private String keyword; // 추후에 검색을 위한 키워드 생성
     private String region;
