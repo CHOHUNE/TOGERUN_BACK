b0aefec311966a4a162f63a8981979eb6141471e
CHOHUNE <yuureru@gmail.com>
2024-10-22 11:35:33 +0900

    feat : githubAction 일부 수정(18) TEST

--- src/main/java/com/example/simplechatapp/util/CustomJWTException.java
+++ src/main/java/com/example/simplechatapp/util/CustomJWTException.java
@@ -3,6 +3,7 @@ package com.example.simplechatapp.util;
 public class CustomJWTException extends RuntimeException{
 
     public CustomJWTException(String message) {
+
         super(message);
     }
 }
